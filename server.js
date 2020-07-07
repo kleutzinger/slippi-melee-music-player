eb9e402569424360b9f1352fcbd4f2635e89a7d1
kleutzinger <thedookmaster@gmail.com>
2020-07-06 23:37:16 -0700

    move to web client

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 var open = require('open');
 
 // opens the url in the default browser
-open('http://sindresorhus.com');
-
 // specify the app to open in
 // opn('http://sindresorhus.com', {app: 'firefox'});
+
+var express = require('express'),
+  app = express(),
+  port = process.env.PORT || 5669;
+
+app.use(express.static(__dirname + '/web'));
+app.use('/sounds', express.static(__dirname + '/sounds'));
+
+app.listen(port);
+
+// open(`http://localhost:${port}`);
--- web/client.js
+++ web/client.js
@@ -9,8 +9,50 @@ $('input').on('change', function(e) {
     var reader = new FileReader();
     reader.onload = function(e) {
       $audio.attr('src', e.target.result);
-      $audio.play();
+      $audio.trigger('play');
     };
     reader.readAsDataURL(file);
   }
+});
+
+let bf_song = { loop: '/sounds/battlefield.ogg' };
+let fountain_song = {
+  loop  : '/sounds/fountain.ogg',
+  intro : '/sounds/fountain-intro.ogg'
+};
+
+let startSong = (songObj) => {
+  if (songObj.intro) {
+    $audio.on('ended', function() {
+      $audio.unbind('end');
+      $audio.attr('src', songObj.loop);
+      $audio.trigger('play');
+      $audio.attr('loop', true);
+    });
+    $audio.attr('src', songObj.intro);
+    $audio.attr('loop', false);
+    $audio.trigger('play');
+  } else {
+    $audio.unbind('end');
+
+    $audio.attr('src', songObj.loop);
+    $audio.trigger('play');
+    $audio.attr('loop', true);
+  }
+};
+
+let stopSong = () => {
+  $audio.trigger('pause');
+};
+
+startSong(fountain_song);
+let $h1 = $('<h1 onclick="$(this).remove()">click to init</h1>');
+
+$(document).ready(function() {
+  $('<p>Hello</p>').appendTo('body');
+  $h1.appendTo('body');
+});
+
+$('body').one('click', function() {
+  setSong(bf_song);
 });
