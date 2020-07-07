0359bbfdaa5710d5cb3f4ef355fa57f3b654b854
Kevin Leutzinger <me@kevinleutzinger.com>
2020-07-06 20:40:46 -0700

    start moving to web client
    oggs also, oggs let you gaplessly loop more easily

---
+++ server.js
@@ -0,0 +1,7 @@
+var open = require('open');
+
+// opens the url in the default browser
+open('http://sindresorhus.com');
+
+// specify the app to open in
+// opn('http://sindresorhus.com', {app: 'firefox'});
